5b0e7fc69dc37401172fc7e91e76c5361597d22a
omisc <o.mischou@gmail.com>
2025-02-02 16:16:41 +0100

    first

---
+++ src/main/java/net/dannthewild/tutorialmod/TutorialModClient.java
@@ -0,0 +1,10 @@
+package net.dannthewild.tutorialmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TutorialModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
